f2d54a27942df47ac92e610f10df6871217f94c1
Marco Ceppi <marco@ceppi.net>
2014-12-23 09:41:36 -0500

    suspend/resume

--- jujulocal/cli.py
+++ jujulocal/cli.py
@@ -1,11 +1,17 @@
 import click
+import logging
 
 from .images import images_cli
+from .suspend import suspend_cli, resume_cli
 
 
 @click.group()
-def main():
+@click.option('-v', '--verbose', count=True)
+@click.option('-q', '--quiet', )
+def main(quiet, verbose):
     pass
 
 
 main.add_command(images_cli)
+main.add_command(suspend_cli)
+main.add_command(resume_cli)
--- jujulocal/suspend.py
+++ jujulocal/suspend.py
@@ -11,26 +11,25 @@ from .helpers import sudo
 
 
 @click.command('suspend')
-@click.option('-e', envvar='JUJU_ENV', help='juju environment')
+@click.option('-e', 'env', envvar='JUJU_ENV', help='juju environment')
 def suspend_cli(env=None):  # pragma: no cover
     try:
         suspend(env)
     except Exception as e:
-        return puts(colored.red('Failed to suspend {}'.format(e))
+        return puts(colored.red('Failed to suspend {}'.format(e)))
 
-    puts(colored.green('{} has been suspended'.format(env))
-
+    puts(colored.green('{} has been suspended'.format(env)))
 
 
 @click.command('resume')
-@click.option('-e', envvar='JUJU_ENV', help='juju environment')
+@click.option('-e', 'env', envvar='JUJU_ENV', help='juju environment')
 def resume_cli(env=None):  # pragma: no cover
     pass
 
 
-def suspend(env)
+def suspend(env):
     pass
 
 
-def resume(env)
+def resume(env):
     pass
